1f0c609def5cff6b7bc16ddeefafb1654b11cb71
John Mark Bell <jmb@netsurf-browser.org>
2008-11-21 22:17:04 +0000

    Something approximating line-height
    TODO: parse floats
    TODO: values with units may contain floats, not integers
    
    svn path=/trunk/libcss/; revision=5761

--- src/parse/css21props.c
+++ src/parse/css21props.c
@@ -1920,10 +1920,72 @@ css_error parse_line_height(css_css21 *c,
 		const parserutils_vector *vector, int *ctx, 
 		css_style **result)
 {
-	UNUSED(c);
-	UNUSED(vector);
-	UNUSED(ctx);
-	UNUSED(result);
+	css_error error;
+	const css_token *token;
+	uint8_t flags = 0;
+	uint16_t value = 0;
+	uint32_t opv;
+	uint32_t length = 0;
+	uint32_t unit = 0;
+	uint32_t required_size;
+
+	/* number | length | percentage | IDENT(normal, inherit) */
+	token = parserutils_vector_peek(vector, *ctx);
+	if (token == NULL)
+		return CSS_INVALID;
+
+	if (token->type == CSS_TOKEN_IDENT &&
+			token->lower.ptr == c->strings[INHERIT]) {
+		parserutils_vector_iterate(vector, ctx);
+		flags = FLAG_INHERIT;
+	} else if (token->type == CSS_TOKEN_IDENT &&
+			token->lower.ptr == c->strings[NORMAL]) {
+		parserutils_vector_iterate(vector, ctx);
+		value = LINE_HEIGHT_NORMAL;
+	} else if (token->type == CSS_TOKEN_NUMBER) {
+		/** \todo length should be a float */
+		size_t consumed = 0;
+		length = integer_from_css_string(&token->lower, &consumed);
+		if (consumed != token->lower.len)
+			return CSS_INVALID;
+
+		value = LINE_HEIGHT_NUMBER;
+	} else {
+		error = parse_unit_specifier(c, vector, ctx, &length, &unit);
+		if (error != CSS_OK)
+			return error;
+
+		if (unit & UNIT_ANGLE || unit & UNIT_TIME || unit & UNIT_FREQ)
+			return CSS_INVALID;
+
+		value = LINE_HEIGHT_DIMENSION;
+	}
+
+	error = parse_important(c, vector, ctx, &flags);
+	if (error != CSS_OK)
+		return error;
+
+	opv = buildOPV(OP_LINE_HEIGHT, flags, value);
+
+	required_size = sizeof(opv);
+	if (value == LINE_HEIGHT_NUMBER)
+		required_size += sizeof(length);
+	else if (value == LINE_HEIGHT_DIMENSION)
+		required_size += sizeof(length) + sizeof(unit);
+
+	/* Allocate result */
+	error = css_stylesheet_style_create(c->sheet, required_size, result);
+	if (error != CSS_OK)
+		return error;
+
+	/* Copy the bytecode to it */
+	memcpy((*result)->bytecode, &opv, sizeof(opv));
+	if (value == LINE_HEIGHT_NUMBER || value == LINE_HEIGHT_DIMENSION)
+		memcpy(((uint8_t *) (*result)->bytecode) + sizeof(opv),
+				&length, sizeof(length));
+	if (value == LINE_HEIGHT_DIMENSION)
+		memcpy(((uint8_t *) (*result)->bytecode) + sizeof(opv) +
+				sizeof(length), &unit, sizeof(unit));
 
 	return CSS_OK;
 }
