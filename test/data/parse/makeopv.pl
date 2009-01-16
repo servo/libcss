aee21231bd45b128b8e48a21dfca57f308a453ce
John Mark Bell <jmb@netsurf-browser.org>
2009-01-16 10:52:39 +0000

    Use oct() instead of hex(). Can now consume binary input.
    svn path=/trunk/libcss/; revision=6075

--- test/data/parse/makeopv.pl
+++ test/data/parse/makeopv.pl
@@ -7,5 +7,5 @@ die "Usage: makeopv.pl <opcode> <flags> <value>\n" if (@ARGV != 3);
 
 my ($opcode, $flags, $value) = @ARGV;
 
-printf("0x%.8x\n", hex($opcode) | hex($flags) << 10 | hex($value) << 18);
+printf("0x%.8x\n", oct($opcode) | oct($flags) << 10 | oct($value) << 18);
 
