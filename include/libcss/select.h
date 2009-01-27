d462fc0cd55b7dbc10e3f08329c4885107a0e59d
John Mark Bell <jmb@netsurf-browser.org>
2009-01-27 09:30:08 +0000

    I guess it helps if you commit the header, too.
    svn path=/trunk/libcss/; revision=6286

--- include/libcss/select.h
+++ include/libcss/select.h
@@ -28,8 +28,12 @@ css_error css_select_ctx_get_sheet(css_select_ctx *ctx, uint32_t index,
 		const css_stylesheet **sheet);
 
 css_error css_select_style(css_select_ctx *ctx, void *node,
-		uint32_t pseudo_classes, uint32_t media,
-		css_computed_style **result);
+		uint64_t pseudo_element, uint64_t pseudo_classes,
+		uint64_t media, css_computed_style *result);
+
+/* css_error css_computed_style_compose(const css_computed_style *parent,
+		const css_computed_style *child,
+		css_computed_style *result); */
 
 /* css_error css_computed_style_destroy(css_computed_style *style); */
 
